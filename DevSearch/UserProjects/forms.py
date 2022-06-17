f45a8e7809353860178a2d96d42d75c404facf70
adam-25 <adarsh.dudhat11@gmail.com>
2022-06-16 19:23:03 -0600

    Add Pre build CSS

--- DevSearch/UserProjects/forms.py
+++ DevSearch/UserProjects/forms.py
@@ -1,4 +1,5 @@
 from django.forms import ModelForm
+from django import forms
 from .models import *
 
 # Form which can be used as a model form to create a new project.
@@ -6,3 +7,12 @@ class ProjectForm(ModelForm):
 	class Meta:
 		model = ProjectsModel
 		fields = ['project_title', 'project_description', 'project_demo', 'source_code', 'project_skills', 'project_image']
+		widgets = {
+			'project_skills': forms.CheckboxSelectMultiple(),
+		}
+
+	def __init__(self, *args, **kwargs):
+		super(ProjectForm, self).__init__(*args, **kwargs)
+		
+		for field in self.fields.values():
+			field.widget.attrs.update({'class': 'input', 'placeholder': field.label + '...', 'autocomplete': 'off'})
